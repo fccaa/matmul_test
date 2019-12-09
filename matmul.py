ec2a781d439654c5a279e21b4180485f4ac9395e
Alan Fung <fccaa33@gmail.com>
2019-12-09 10:54:07 +0900

    Added new statistics

--- matmul.py
+++ matmul.py
@@ -9,8 +9,6 @@ parser.add_argument("-s", metavar="int", type=int, help="Number of Sample", defa
 parser.add_argument("-N", metavar="int", type=int, help="Maximum size of matrix", default=10000)
 parser.add_argument("-fp32", help="Do matrix multiplication with fp32", action="store_true")
 
-
-
 args = parser.parse_args()
 
 N=10
@@ -18,12 +16,21 @@ while N < args.N:
 	GFLOPS = 2.0 * N * N * N / pow(1000.0, 3)
 	A = numpy.random.rand(N,N) + 1.0
 	B = numpy.random.rand(N,N) + 1.0
-	t1 = time.time()
+	time_record = numpy.array([])
+
 	for i in range(args.s):
+		time.sleep(1)
 		if args.fp32:
+			t1 = time.time()
 			C = numpy.matmul(A.astype(numpy.float32), B.astype(numpy.float32))
+			t2 = time.time()
 		else:
+			t1 = time.time()
 			C = numpy.matmul(A.astype(numpy.float64), B.astype(numpy.float64))
-	t2 = time.time()
-	print(N, (GFLOPS * args.s)/(t2 - t1), flush=True)
+			t2 = time.time()
+		time_record = numpy.append(time_record, numpy.fabs(t2-t1))
+
+	perf_max = GFLOPS / time_record.min()
+	perf_avg = (GFLOPS / time_record).mean()
+	print(N, perf_avg, perf_max, flush=True)
 	N = int(N*1.2)
